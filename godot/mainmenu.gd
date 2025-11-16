070d84aec16832e4fe97a1e0dd1b7b29ce68eb10
darpra123 <darko.pracic@gmail.com>
2025-11-16 20:57:28 +0100

    Removed hard coded reference to level.tscn and added it as an exported variable in MainMenu's inspector

--- godot/mainmenu.gd
+++ godot/mainmenu.gd
@@ -1,5 +1,6 @@
 extends MarginContainer
 
+@export var level_scene_path:String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,5 +13,4 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
-	pass # Replace with function body.
+	get_tree().change_scene_to_file(level_scene_path)
